a477350d64e8960a7056125273abb82842d84b08
RafsanBabu <rafsan.cse.ru13@gmail.com>
2023-04-17 22:38:51 +0600

    code formatted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Table from './Component/Table';
+import logo from "./logo.svg";
+import "./App.css";
+import Table from "./Component/Table";
 
 function App() {
   return (
-    <div className='App'>
-      <Table/>
+    <div className="App">
+      <Table />
     </div>
   );
 }
--- src/Component/Table.jsx
+++ src/Component/Table.jsx
@@ -1,70 +1,78 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import DataTable from 'react-data-table-component';
-import { getDefaultNormalizer } from '@testing-library/react';
+import DataTable from "react-data-table-component";
+
+import '../App.css'
+import { getDefaultNormalizer } from "@testing-library/react";
 
 function Table() {
   const [data, setData] = useState([]);
   const [pending, setPending] = useState(true);
+
   const customStyles = {
     rows: {
-        style: {
-            minHeight: '72px', // override the row height
-        },
+      style: {
+        minHeight: "72px", // override the row height
+      },
     },
     headCells: {
-        style: {
-            paddingLeft: '8px', // override the cell padding for head cells
-            paddingRight: '8px',
-            color: 'white',
-            backgroundColor:'rgb(14 241 188)',
-            fontWeight: 'bold',
-            fontSize:'16px'
-        },
+      style: {
+        paddingLeft: "8px", // override the cell padding for head cells
+        paddingRight: "8px",
+        color: "white",
+        backgroundColor: "rgb(14 241 188)",
+        fontWeight: "bold",
+        fontSize: "16px",
+      },
     },
     cells: {
-        style: {
-            paddingLeft: '8px', // override the cell padding for data cells
-            paddingRight: '8px',
-        },
+      style: {
+        paddingLeft: "8px", // override the cell padding for data cells
+        paddingRight: "8px",
+      },
     },
-};
+  };
+
   const column = [
     {
       name: "ID",
-      selector: row => row.id,
+      selector: (row) => row.id,
       sortable: true,
     },
     {
       name: "Name",
-      selector: row => row.firstName.concat(" ").concat(row.lastName),
+      selector: (row) => row.firstName.concat(" ").concat(row.lastName),
       sortable: true,
     },
     {
       name: "Email",
-      selector: row => row.email,
+      selector: (row) => row.email,
       sortable: true,
     },
     {
       name: "City",
-      selector: row => row.address.city,
+      selector: (row) => row.address.city,
       sortable: true,
     },
-  ]
+  ];
 
-  useEffect(() =>{
+  useEffect(() => {
     getData();
-  },[])
-  const  getData = async () => {
-    axios.get("https://dummyjson.com/users")
-    .then(res => {
-      setData(res?.data?.users)
-      console.log(res);
-    setPending(false)})
-    .catch(err => console.log(err));
-  }
+  }, []);
+
+  const getData = async () => {
+    axios
+      .get("https://dummyjson.com/users")
+      .then((res) => {
+        setData(res?.data?.users);
+        console.log(res);
+        setPending(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="text-center container m-5">
+    <div  className="table-content">
       <h3>Dynamic Table</h3>
       <DataTable
         columns={column}
